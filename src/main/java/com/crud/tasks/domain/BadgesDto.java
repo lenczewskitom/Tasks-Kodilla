af3193a187cd67aac8a23ac237faf35967f4a7f3
lenczewskitom <lenczewski.tom@gmail.com>
2022-09-07 08:55:10 +0200

    BadgesDto fixed

--- src/main/java/com/crud/tasks/domain/BadgesDto.java
+++ src/main/java/com/crud/tasks/domain/BadgesDto.java
@@ -1,10 +1,14 @@
 package com.crud.tasks.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BadgesDto {
     private int votes;
     private List<TrelloAttachmentsDto> attachmentsByType;
